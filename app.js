b47e98d877ecd10563c3d91dde20ec13d401c260
Shihab <shihabdinajpur@gmail.com>
2020-04-10 19:49:00 +0600

    Change port

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const app = express()
 app.get('/', function(req, res) {
     res.send("Wow!")
 })
-app.listen(3000)
+
+let port = process.env.PORT
+if(port == null || port == "") {
+    port = 3000
+}
+app.listen(port)
