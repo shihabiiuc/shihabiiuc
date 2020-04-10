3954e1a8fdce3d925640608aa432445f565bc014
Shihab <shihabdinajpur@gmail.com>
2020-04-10 23:32:35 +0600

    Added separate Router file

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
+const router = require('./router')
 
 app.use(express.static('public'))
 
 app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-app.get('/', function(req, res) {
-    res.render('home-guest')
-})
+app.use('/', router)
 
 let port = process.env.PORT
 if(port == null || port == "") {
