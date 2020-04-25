321532deca796c654b46371059271735a863f2a5
Shihab <shihabdinajpur@gmail.com>
2020-04-25 20:10:30 +0600

    removed ssl npm package

--- app.js
+++ app.js
@@ -9,14 +9,6 @@ const paypal = require('paypal-rest-sdk')
 const dotenv = require('dotenv')
 dotenv.config()
 
-// Forcing to ssl
-app.use(function(req, res, next) {
-    if(!req.secure) {
-        return res.redirect(['https://', req.get('Host'), req.baseUrl].join(''));
-    }
-    next();
-});
-
 paypal.configure({
     'mode': 'live', //sandbox or live
     'client_id': process.env.CLIENTID,
